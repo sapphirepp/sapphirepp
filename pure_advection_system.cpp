e04cc8e656f6eb8334e8a075281c054e5e57b7fa
Nils Schween <nils.schween@mpi-hd.mpg.de>
2021-09-06 17:42:30 +0200

    Project setup

---
+++ pure_advection_system.cpp
@@ -0,0 +1,27 @@
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/tensor.h>
+
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/sparse_matrix.h>
+
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_generator.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_dgq.h>
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+  std::cout << "This is a test" << "\n";
+  
+  return 0;
+}
