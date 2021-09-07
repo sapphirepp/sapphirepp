7c919c733a7194d88c4ee1902ad5f4e6baa6aa77
Nils Schween <nils.schween@mpi-hd.mpg.de>
2021-09-07 17:27:58 +0200

    added missing header files; created simulation class and defined members

--- pure_advection_system.cpp
+++ pure_advection_system.cpp
@@ -1,27 +1,93 @@
-#include <deal.II/base/quadrature_lib.h>
 #include <deal.II/base/function.h>
-#include <deal.II/base/tensor.h>
+#include <deal.II/base/quadrature_lib.h>
+// #include <deal.II/base/tensor.h>
 
-#include <deal.II/lac/vector.h>
-#include <deal.II/lac/full_matrix.h>
-#include <deal.II/lac/sparse_matrix.h>
-
-#include <deal.II/grid/tria.h>
 #include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/tria.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/precondition_block.h>
+#include <deal.II/lac/solver_richardson.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/vector.h>
+// #include <deal.II/grid/grid_refinement.h>
 
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_tools.h>
-
-#include <deal.II/fe/fe_values.h>
-#include <deal.II/fe/fe_system.h>
 #include <deal.II/fe/fe_dgq.h>
+#include <deal.II/fe/fe_interface_values.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/mapping_q1.h>
+#include <deal.II/meshworker/mesh_loop.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/vector_tools.h>
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-int main()
-{
-  std::cout << "This is a test" << "\n";
+namespace pure_advection_system {
+using namespace dealii;
+
+template <int max_degree, int dim>
+class PureAdvection {
+ public:
+  PureAdvection();
+  void run();
+
+ private:
+  void setup_system();
+  void make_grid();
+  void assemble_system();
+  void solve_system();
+  void output_results() const;
+  void output_parameters() const;
+
+  Triangulation<dim> triangulation;
+  DoFHandler<dim> dof_handler;
+
+  // NOTE: The explicit use of a mapping is most likely related to the usage of
+  // mesh_loop as well
+  const MappingQ1<dim> mapping;
+  const FESystem<dim> fe;  // TODO: const is probably wrong
+
+  // NOTE: Quadratures are members of this class (and not e.g. part of the
+  // assemble_system method), because I am using the mesh_loop function
+  const QGauss<dim> quadrature;
+  const QGauss<dim - 1> quadrature_face;
+
+  AffineConstraints<double> constraints;  // used in the L_2 projection of the
+  // intial condition onto the finite
+  // element space
+
+  SparsityPattern sparsity_pattern;
+  SparseMatrix<double> mass_matrix;
+  SparseMatrix<double> dg_matrix;
+  SparseMatrix<double> system_matrix;
   
+  Vector<double> current_solution;
+  Vector<double> previous_solution;
+
+  Vector<double> system_rhs;
+
+  // number of expansion coefficients
+  unsigned int num_modes = (max_degree + 1) * (max_degree + 1);
+
+  double time_step;
+  double time;
+  unsigned int time_step_number;
+  const double theta;
+  // penalty parameter ( for a scalar equation eta = 1 -> upwinding)
+  double eta;
+  // Fort the moment, I will use a quadratic domain
+  // Rectangular domain
+  // Point<dim> left_bottom;
+  // Point<dim> right_top;
+};
+
+}  // namespace pure_advection_system
+int main() {
+
   return 0;
 }
