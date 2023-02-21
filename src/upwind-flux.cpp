f89eb77fe5c39062cac2caeabcde6dad2059080f
Nils Schween <nils.schween@mpi-hd.mpg.de>
2023-02-21 10:13:31 +0100

    flatten the for loop in matrix_sum

--- src/upwind-flux.cpp
+++ src/upwind-flux.cpp
@@ -27,6 +27,11 @@ VFPEquation::UpwindFlux<dim>::UpwindFlux(const PDESystem &system)
   for (unsigned int k = 0; k < 3; ++k) {
     for (unsigned int i = 0; i < matrix_size; ++i)
       for (unsigned int j = 0; j < matrix_size; ++j)
+        // NOTE: When computing the eigenvalues of matrix_sum, I call a Fortran
+        // routine which requires the matrix elements to be stored in
+        // column-major order. But since the resulting matrix is symmteric,
+        // there is no difference between column-major order and row-major
+        // order. I stick with the C convention and use row-major order.
         advection_matrices[k][i * matrix_size + j] = adv_mat[k](i, j);
   }
   const std::vector<dealii::LAPACKFullMatrix<double>> &adv_mat_prod =
@@ -43,30 +48,18 @@ void VFPEquation::UpwindFlux<dim>::compute_matrix_sum(
     const double n_p, const double p, const double gamma,
     const dealii::Vector<double> &material_derivative,
     const std::vector<dealii::Vector<double>> &jacobian) {
-  for (unsigned int j = 0; j < matrix_size; ++j)
-    for (unsigned int i = 0; i < matrix_size; ++i)
-      // NOTE: When computing the eigenvalues of matrix_sum, I call a Fortran
-      // routine which requires the matrix elements to be stored in column-major
-      // order. But since the resulting matrix is symmteric, there is no
-      // difference between column-major order and row-major order. I stick with
-      // the C convention and use row-major order.
-      matrix_sum[i * matrix_size + j] =
-          -n_p *
-          (gamma * (material_derivative[0] *
-                        advection_matrices[0][i * matrix_size + j] +
-                    material_derivative[1] *
-                        advection_matrices[1][i * matrix_size + j] +
-                    material_derivative[2] *
-                        advection_matrices[2][i * matrix_size + j]) +
-           p * (jacobian[0][0] * adv_mat_products[0][i * matrix_size + j] +
-                jacobian[1][1] * adv_mat_products[3][i * matrix_size + j] +
-                jacobian[2][2] * adv_mat_products[5][i * matrix_size + j] +
-                (jacobian[0][1] + jacobian[1][0]) *
-                    adv_mat_products[1][i * matrix_size + j] +
-                (jacobian[0][2] + jacobian[2][0]) *
-                    adv_mat_products[2][i * matrix_size + j] +
-                (jacobian[1][2] + jacobian[2][1]) *
-                    adv_mat_products[4][i * matrix_size + j]));
+  for (unsigned int i = 0; i < matrix_size * matrix_size; ++i)
+    matrix_sum[i] =
+        -n_p *
+        (gamma * (material_derivative[0] * advection_matrices[0][i] +
+                  material_derivative[1] * advection_matrices[1][i] +
+                  material_derivative[2] * advection_matrices[2][i]) +
+         p * (jacobian[0][0] * adv_mat_products[0][i] +
+              jacobian[1][1] * adv_mat_products[3][i] +
+              jacobian[2][2] * adv_mat_products[5][i] +
+              (jacobian[0][1] + jacobian[1][0]) * adv_mat_products[1][i] +
+              (jacobian[0][2] + jacobian[2][0]) * adv_mat_products[2][i] +
+              (jacobian[1][2] + jacobian[2][1]) * adv_mat_products[4][i]));
   // matrix_sum[i * matrix_size + j] =
   //     -n_p *
   //     (gamma * (material_derivative[0] * advection_matrices[0](i, j) +
