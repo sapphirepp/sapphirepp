61cd3ce17ab67e23e617c2d6f76be97fe5c85d7f
Nils Schween <nils.schween@mpi-hd.mpg.de>
2023-02-21 17:59:27 +0100

    some progress in implementing the upwind flux class

--- include/upwind-flux.h
+++ include/upwind-flux.h
@@ -14,8 +14,8 @@
 namespace VFPEquation {
 template <int dim>
 class UpwindFlux {
-  UpwindFlux(const PDESystem& system);
-  void set_time();
+  UpwindFlux(const PDESystem& system, bool momentum);
+  void set_time(double time);
   void compute_upwind_fluxes(
       const std::vector<dealii::Point<dim>>& q_points,
       const std::vector<dealii::Tensor<1, dim>>& normals,
@@ -23,25 +23,50 @@ class UpwindFlux {
       std::vector<dealii::FullMatrix<double>>& negative_flux_matrices);
 
  private:
-  void compute_eigenvalues_and_eigenvectors(const char range, const double vl,
-                                            const double vu,
-                                            const double abstol);
+  // void compute_eigenvalues_and_eigenvectors(std::vector<double>&
+  // matrix_values);
   void prepare_upwind_fluxes();
   void separate_positive_and_negative_eigenvalues();
-  void triple_product(dealii::FullMatrix<double>& flux_matrix);
+  void triple_product(const std::vector<double>& eigenvalues,
+                      const std::vector<double>& eigenvectors,
+                      dealii::FullMatrix<double>& flux_matrix);
   void compute_flux_in_space_directions();
   void compute_matrix_sum(const double n_p, const double p, const double gamma,
                           const dealii::Vector<double>& material_derivative,
                           const std::vector<dealii::Vector<double>>& jacobian);
   void compute_flux_in_p_direction();
-  // The computation of the upwind flux in the p direction requires to the
-  // compute eigenvalues and eigenvectors for every face with n_p = 1 (or -1).
-  // Lapack needs correctly sized work arrays to do these computations. It can
-  // determine the sizes it needs with a specific call to the used eigenvalue
-  // routine. This is done in constructor of this class. The work arrays will be
-  // allocated only once and then reused every time eigenvalues and eigenvectors
-  // are computed.
-  dealii::types::blas_int info;  // Lapack's error code
+
+  std::vector<std::vector<double>> advection_matrices;
+  std::vector<std::vector<double>> adv_mat_products;
+
+  // Will be used and overwritten in flux computations in p directions
+  std::vector<double> matrix_sum;
+  std::vector<double> eigenvalues;
+  std::vector<double> eigenvectors;
+  int num_eigenvalues;
+  std::vector<double> positive_eigenvalues;
+  std::vector<double> negative_eigenvalues;
+
+  // Are used in the flux computations in space directions
+  std::vector<double> eigenvalues_advection_matrices;
+  std::vector<std::vector<double>> eigenvectors_advection_matrices;
+
+  // Physical input
+  BackgroundVelocityField<dim> background_velocity_field;
+  ParticleVelocity<dim> particle_velocity_func;
+  ParticleGamma<dim> particle_gamma_func;
+  ParticleProperties particle_properties;
+  // PDE System matrices
+  const int matrix_size;
+  const PDESystem& pde_system;
+
+  // Arguments for the Lapack routine xsyevr
+  // Documentation:
+  // https://netlib.org/lapack/explore-html/d3/d88/group__real_s_yeigen_ga24155d2da67fb4a896c5f8257589b19f.html#ga24155d2da67fb4a896c5f8257589b19f
+  // https://www.intel.com/content/www/us/en/develop/documentation/onemkl-developer-reference-fortran/top/lapack-routines/lapack-least-squares-and-eigenvalue-problem/lapack-least-squares-eigenvalue-problem-driver/symmetric-eigenvalue-problems-lapack-driver/syevr.html#syevr
+
+  // Lapack's error code
+  dealii::types::blas_int info;
   // working array containting doubles
   std::vector<double> work;
   // length of the working array -> to be  determined by the first call of syevr
@@ -49,27 +74,41 @@ class UpwindFlux {
   // working array containing integers
   std::vector<dealii::types::blas_int> iwork;
   dealii::types::blas_int liwork;
+  // SUPPZ is INTEGER array, dimension at least ( 2*max(1,matrix_size) )
+  // The support of the eigenvectors in "eigenvectors" array, i.e., the indices
+  // indicating the nonzero elements in "eigenvectors" array
+  std::vector<dealii::types::blas_int> isuppz;
+  // jobz = 'V' means to compute eigenvalues and eigenvectors
+  const char* const jobz;
+  // range = 'A' means to compute *all* eigenvalues and eigenvectors
+  const char* const range;
+  // the input array is an upper triangular matrix
+  const char* const uplo;
+  // The absolute error tolerance for the eigenvalues.
+  //
+  // An approximate eigenvalue is accepted as converged when it is determined to
+  // lie in an interval [a,b] of width less than or equal to
+  //                 ABSTOL + EPS *   max( |a|,|b| ) ,
+  // where EPS is the machine precision. If ABSTOL is less than or equal to
+  // zero, then EPS*|T| will be used in its place, where |T| is the 1-norm of
+  // the tridiagonal matrix obtained by reducing A to tridiagonal form.
+  const double abstol;
+  // If range = 'A', no upper, nor lower bounds for the eigenvalues are needed.
+  // It is still necessary to hand over dummies, which are not referenced when
+  // the routine is called.
+  const dealii::types::blas_int* const int_dummy;
+  const double double_dummy;
 
-  std::vector<std::vector<double>> advection_matrices;
-  std::vector<std::vector<double>> adv_mat_products;
-
-  // Will be overwritten in flux computations
-  std::vector<double> matrix_sum;
-  std::vector<double> eigenvalues;
-  std::vector<double> eigenvectors;
-  std::vector<double> positive_eigenvalues;
-  std::vector<double> negative_eigenvalues;
-
-  // Are used in the flux computations in space directions
-  std::vector<dealii::LAPACKFullMatrix<double>> eigenvectors_advection_matrices;
-  std::vector<double> eigenvalues_advection_matrices;
-
-  // Physical input
-  BackgroundVelocityField<dim> background_velocity_field;
-  ParticleGamma<dim> particle_gammas;
-
-  // PDE System matrices
-  const unsigned int matrix_size;
-  const PDESystem& pde_system;
+  // The upwind flux computations require to know if the momentum direction
+  // exits or not.
+  const bool momentum;
+  // Enum to ease the understanding in the compute_upwind_flux function
+  enum Coordinate {
+    x,
+    y,
+    z,
+    p,  // p is always the component of in Point<dim> etc.
+    none = 1000
+  };
 };
 }  // namespace VFPEquation
--- src/upwind-flux.cpp
+++ src/upwind-flux.cpp
@@ -1,10 +1,12 @@
 #include "upwind-flux.h"
 
-#include <deal.II/lac/lapack_full_matrix.h>
+#include <deal.II/base/exceptions.h>
+#include <deal.II/lac/lapack_support.h>
+#include <deal.II/lac/lapack_templates.h>  // direct access to the Fortran driver routines of Lapack
 #include <deal.II/lac/vector.h>
 
 template <int dim>
-VFPEquation::UpwindFlux<dim>::UpwindFlux(const PDESystem &system)
+VFPEquation::UpwindFlux<dim>::UpwindFlux(const PDESystem &system, bool momentum)
     : pde_system{system},
       matrix_size(pde_system.system_size()),
       advection_matrices(3),
@@ -14,10 +16,17 @@ VFPEquation::UpwindFlux<dim>::UpwindFlux(const PDESystem &system)
       eigenvectors(matrix_size * matrix_size),
       positive_eigenvalues(matrix_size),
       negative_eigenvalues(matrix_size),
-      eigenvectors_advection_matrices(3),
+      eigenvectors_advection_matrices(
+          3, std::vector<double>(matrix_size * matrix_size)),
       eigenvalues_advection_matrices(matrix_size),
-      lwork{-1},
-      liwork{-1} {
+      isuppz(2 * matrix_size),
+      jobz{&dealii::LAPACKSupport::V},
+      range{&dealii::LAPACKSupport::A},
+      uplo{&dealii::LAPACKSupport::U},
+      abstol{0.},
+      int_dummy{&dealii::LAPACKSupport::one},
+      double_dummy{1.},
+      momentum{momentum} {
   // NOTE: Since we very often call compute_matrix_sum and the matrixes classes
   // of dealii do not allow unchecked access to there raw data, we create copies
   // of the matrices in the hope that this additional memory consumption is made
@@ -25,8 +34,8 @@ VFPEquation::UpwindFlux<dim>::UpwindFlux(const PDESystem &system)
   const std::vector<dealii::LAPACKFullMatrix<double>> &adv_mat =
       pde_system.get_advection_matrices();
   for (unsigned int k = 0; k < 3; ++k) {
-    for (unsigned int i = 0; i < matrix_size; ++i)
-      for (unsigned int j = 0; j < matrix_size; ++j)
+    for (int i = 0; i < matrix_size; ++i)
+      for (int j = 0; j < matrix_size; ++j)
         // NOTE: When computing the eigenvalues of matrix_sum, I call a Fortran
         // routine which requires the matrix elements to be stored in
         // column-major order. But since the resulting matrix is symmteric,
@@ -37,18 +46,238 @@ VFPEquation::UpwindFlux<dim>::UpwindFlux(const PDESystem &system)
   const std::vector<dealii::LAPACKFullMatrix<double>> &adv_mat_prod =
       pde_system.get_adv_mat_products();
   for (unsigned int k = 0; k < 6; ++k) {
-    for (unsigned int i = 0; i < matrix_size; ++i)
-      for (unsigned int j = 0; j < matrix_size; ++j)
+    for (int i = 0; i < matrix_size; ++i)
+      for (int j = 0; j < matrix_size; ++j)
         adv_mat_products[k][i * matrix_size + j] = adv_mat_prod[k](i, j);
   }
+  // Preparations for the eigenvalue and eigenvector computations
+  //
+  // The computation of the upwind flux in the p direction requires to the
+  // compute eigenvalues and eigenvectors for every face with n_p = 1 (or -1).
+  // Lapack needs correctly sized work arrays to do these computations. It can
+  // determine the sizes it needs with a specific call to the used eigenvalue
+  // routine. The work arrays will be allocated only once and then reused every
+  // time eigenvalues and eigenvectors are computed.
+
+  // https://stackoverflow.com/questions/46618391/what-is-the-use-of-the-work-parameters-in-lapack-routines
+  // Quote: "If dynamic allocation is available, the most common use of LAPACK
+  // functions is to first perform a workspace query using LWORK = -1, then use
+  // the return value to allocate a WORK array of the correct size and finally
+  // call the routine of LAPACK to get the expected result. High-end wrappers of
+  // LAPACK such as LAPACKE features function doing just that: take a look at
+  // the source of LAPACKE for function LAPACKE_dsyev()! It calls twice the
+  // function LAPACKE_dsyev_work, which calls LAPACK_dsyev (wrapping dsyev()).
+
+  // Wrappers still feature functions such as LAPACKE_dsyev_work(), where the
+  // arguments work and lwork are still required. The number of allocations can
+  // therefore be reduced if the routine is called multiple times on similar
+  // sizes by not deallocating WORK between calls, but the user must do that
+  // himself (see this example). In addition, the source of ILAENV, the function
+  // of LAPACK called to compute the optimzed size of WORK, features the
+  // following text:
+
+  //     This version provides a set of parameters which should give good, but
+  //     not optimal, performance on many of the currently available computers.
+  //     Users are encouraged to modify this subroutine to set the tuning
+  //     parameters for their particular machine using the option and problem
+  //     size information in the arguments.
+
+  // As a result, testing sizes of WORK larger than the size returned by the
+  // workspace query could improve performances."
+
+  // Compute the size of the working arrays for the Lapack routine xsyevr
+
+  // The optimal size of the work arrays will be stored in their first element
+  work.resize(1);
+  iwork.resize(1);
+  lwork = -1;
+  liwork = -1;
+  // create dummy variables for the matrix sum
+  double n_p_dummy = 1.;
+  double gamma_dummy = 1.;
+  double p_dummy = 1.;
+  dealii::Vector<double> material_derivative_dummy{1., 1., 1.};
+  std::vector<dealii::Vector<double>> jacobian_dummy(
+      3, dealii::Vector<double>{1., 1., 1.});
+  compute_matrix_sum(n_p_dummy, p_dummy, gamma_dummy, material_derivative_dummy,
+                     jacobian_dummy);
+  // call Lapack routine
+  dealii::syevr(jobz, range, uplo, &matrix_size, matrix_sum.data(),
+                &matrix_size, &double_dummy, &double_dummy, int_dummy,
+                int_dummy, &abstol, &num_eigenvalues, eigenvalues.data(),
+                eigenvectors.data(), &matrix_size, isuppz.data(), work.data(),
+                &lwork, iwork.data(), &liwork, &info);
+  Assert(info == 0, dealii::ExcInternalError());
+
+  // resize the working arrays
+  lwork = static_cast<dealii::types::blas_int>(work[0]);
+  liwork = iwork[0];
+  work.resize(lwork);
+  iwork.resize(liwork);
+
+  // Prepare upwind fluxes in space directions
+  prepare_upwind_fluxes();
 }
+
+template <int dim>
+void VFPEquation::UpwindFlux<dim>::set_time(double time) {
+  background_velocity_field.set_time(time);
+}
+
+template <int dim>
+void VFPEquation::UpwindFlux<dim>::compute_upwind_fluxes(
+    const std::vector<dealii::Point<dim>> &q_points,
+    const std::vector<dealii::Tensor<1, dim>> &normals,
+    std::vector<dealii::FullMatrix<double>> &positive_flux_matrices,
+    std::vector<dealii::FullMatrix<double>> &negative_flux_matrices) {
+  // Determine if we are computing the flux of an interior face whose normal
+  // points into the x,y,z or p direction. Since we are using a rectangular
+  // grid, the normal is the same for all quadrature points and it points either
+  // in the x, y, z or p direction. Hence it can determined outside the loop
+  // over the quadrature points
+
+  unsigned int dim_cs = dim - momentum;
+  Coordinate component = none;  // Produces an error if not overwritten
+
+  for (unsigned int i = 0; i < dim_cs; ++i)
+    // NOTE: Such a simple comparison of doubles is only possible, because I
+    // know that I am comparing 0. with 1. or something very close to one with
+    // one. If this ever fails a more sophisticated comparison is needed, e.g.
+    // https://floating-point-gui.de/errors/comparison/
+    // std::abs is nessecary because of the boundary faces, i.e. <-| n_k = -1
+    // for some faces.
+    if (std::abs(1. - std::abs(normals[0][i])) < 1e-5) component = i;
+
+  // If the momentum terms are included, then the last component of normals is
+  // the momentum direction
+  if (momentum && std::abs(1. - std::abs(normals[0][dim])) < 1e-5)
+    component = p;
+
+  // Flux in the spatial directions
+  if (component == x || component == y || component == z) {
+    // Background velocity field
+    // Get the value of the velocity field at every quadrature point
+    // TODO: Value list for a specific component would be faster.
+    std::vector<dealii::Vector<double>> velocities(q_points.size(),
+                                                   dealii::Vector<double>(3));
+    background_velocity_field.vector_value_list(q_points, velocities);
+
+    // Particle
+    std::vector<double> particle_velocities(q_points.size());
+    if (momentum) {
+      // if distribution functions depends on p, the particle velocity depends
+      // on the position in the phase space and needs to be computed
+      particle_velocity_func.value_list(q_points, particle_velocities);
+    } else {
+      std::fill(particle_velocities.begin(), particle_velocities.end(),
+                particle_properties.velocity);
+    }
+    // Compute flux at every quadrature point
+    for (unsigned int q_index; q_index < q_points.size(); ++q_index)
+      compute_flux_in_space_directions(
+          normals[component], velocities[q_index], particle_velocities[q_index],
+          positive_flux_matrices[q_index], negative_flux_matrices[q_index]);
+  } else if (component == p) {
+    std::vector<dealii::Vector<double>> material_derivative_vel(
+        q_points.size(), dealii::Vector<double>(3));
+    background_velocity_field.material_derivative_list(q_points,
+                                                       material_derivative_vel);
+
+    std::vector<std::vector<dealii::Vector<double>>> jacobians_vel(
+        q_points.size(),
+        std::vector<dealii::Vector<double>>(3, dealii::Vector<double>(3)));
+    background_velocity_field.jacobian_list(q_points, jacobians_vel);
+
+    std::vector<double> particle_gammas(q_points.size());
+    particle_gamma_func.value_list(q_points, particle_gammas);
+    for (unsigned int q_index = 0; q_index < q_points.size(); ++q_index)
+      compute_flux_in_p_direction(
+          normals[p], q_points[p], particle_gammas[q_index],
+          material_derivative_vel[q_index], jacobians_vel[q_index]);
+  }
+}
+
+template <int dim>
+void VFPEquation::UpwindFlux<dim>::prepare_upwind_fluxes() {
+  // The eigenvalues of A_x are also the eigenvalues of A_y and A_z. The
+  // eigenvalues of A_x are the roots of the associated legendre polynomials.
+  // Since the associated Legendre Polynomials are orthogonal polynomials, it
+  // can be shown that all roots are contained in in the intervall [-1., 1.].
+  // Physically this makes sense, because c = 1 and the eigenvalues encode the
+  // speed of 'information' transport.
+  //
+  // Helper variables
+  const char *const V = &dealii::LAPACKSupport::V;
+  const double vl = -1.1;
+  const double vu = 1.1;
+  // NOTE: We need another copy of the advection matrix, because it gets
+  // destroyed when the xsyevr is called.
+  std::vector<double> A_x;
+  A_x = advection_matrices[0];
+  dealii::syevr(jobz, V, uplo, &matrix_size, A_x.data(), &matrix_size, &vl, &vu,
+                int_dummy, int_dummy, &abstol, &num_eigenvalues,
+                eigenvalues_advection_matrices.data(),
+                eigenvectors_advection_matrices[0].data(), &matrix_size,
+                isuppz.data(), work.data(), &lwork, iwork.data(), &liwork,
+                &info);
+  // NOTE: The eigenvalues of A_x can computed very efficiently because A_x is
+  // (when ordered correctly) a tridiagonal symmetric matrix, i.e. it is in
+  // Hessenberg-Form. At the moment it is not ordered correctly. The computation
+  // has only to be done once.
+  Assert(info >= 0, dealii::ExcInternalError());
+  if (info != 0)
+    std::cerr << "The computation of the eigenvalues and eigenvectors of A_x "
+                 "failed: LAPACK error in syevr"
+              << std::endl;
+  std::vector<double> A_y;
+  A_y = advection_matrices[1];
+  dealii::syevr(jobz, V, uplo, &matrix_size, A_y.data(), &matrix_size, &vl, &vu,
+                int_dummy, int_dummy, &abstol, &num_eigenvalues,
+                eigenvalues_advection_matrices.data(),
+                eigenvectors_advection_matrices[1].data(), &matrix_size,
+                isuppz.data(), work.data(), &lwork, iwork.data(), &liwork,
+                &info);
+  Assert(info >= 0, dealii::ExcInternalError());
+  if (info != 0)
+    std::cerr << "The computation of the eigenvalues and eigenvectors of A_y "
+                 "failed: LAPACK error in syevr"
+              << std::endl;
+
+  std::vector<double> A_z;
+  A_z = advection_matrices[2];
+  dealii::syevr(jobz, V, uplo, &matrix_size, A_z.data(), &matrix_size, &vl, &vu,
+                int_dummy, int_dummy, &abstol, &num_eigenvalues,
+                eigenvalues_advection_matrices.data(),
+                eigenvectors_advection_matrices[2].data(), &matrix_size,
+                isuppz.data(), work.data(), &lwork, iwork.data(), &liwork,
+                &info);
+  Assert(info >= 0, dealii::ExcInternalError());
+  if (info != 0)
+    std::cerr << "The computation of the eigenvalues and eigenvectors of A_z "
+                 "failed: LAPACK error in syevr"
+              << std::endl;
+
+  // TODO: Rotate the eigenvectors of A_x to get the eigenvectors of A_y and
+  // A_z, i.e. implement the rotation matrices e^{-i\Omega_x pi/2} and e^{-i
+  // \Omega_z pi/2} For now we will three times compute the same eigenvalues to
+  // get the eigenvectors of A_y and A_z
+}
+// template <int dim>
+// void VFPEquation::UpwindFlux<dim>::compute_eigenvalues_and_eigenvectors(
+//     std::vector<double> &matrix_values) {
+//   dealii::syevr(jobz, range, uplo, &matrix_size, matrix_values.data(),
+//                 &matrix_size, &double_dummy, &double_dummy, int_dummy,
+//                 int_dummy, &abstol, &num_eigenvalues, eigenvalues.data(),
+//                 eigenvectors.data(), &matrix_size, isuppz.data(),
+//                 work.data(), &lwork, iwork.data(), &liwork, &info);
+// }
 
 template <int dim>
 void VFPEquation::UpwindFlux<dim>::compute_matrix_sum(
     const double n_p, const double p, const double gamma,
     const dealii::Vector<double> &material_derivative,
     const std::vector<dealii::Vector<double>> &jacobian) {
-  for (unsigned int i = 0; i < matrix_size * matrix_size; ++i)
+  for (int i = 0; i < matrix_size * matrix_size; ++i)
     matrix_sum[i] =
         -n_p *
         (gamma * (material_derivative[0] * advection_matrices[0][i] +
