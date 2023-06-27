756fad32551ebe360b58177d043668da9026b117
Nils Schween <nils.schween@mpi-hd.mpg.de>
2023-06-27 15:07:08 +0200

    fix: adapt the particle functions to the new unit system

--- src/vfp/particle-functions.cpp
+++ src/vfp/particle-functions.cpp
@@ -2,6 +2,8 @@
 
 #include <cmath>
 
+#include "physical-setup.h"
+
 template <int dim>
 void Sapphire::ParticleVelocity<dim>::value_list(
     const std::vector<dealii::Point<dim>> &points,
@@ -9,13 +11,16 @@ void Sapphire::ParticleVelocity<dim>::value_list(
   Assert(velocities.size() == points.size(),
          dealii::ExcDimensionMismatch(velocities.size(), points.size()));
   static_cast<void>(component);
+  ParticleProperties particle_properties;
+
   for (unsigned int i = 0; i < points.size(); ++i) {
     // NOTE: It is assumed that magnitude of p is always the last component of a
     // point (i.e the coordinates of the phase space are x,(y,z), p)
     double p =
         (logarithmic_p ? std::exp(points[i][dim - 1]) : points[i][dim - 1]);
-    velocities[i] = p / std::sqrt(p * p + 1 / (reference_values.gamma *
-                                               reference_values.gamma));
+    velocities[i] = 1. / std::sqrt(particle_properties.mass *
+                                       particle_properties.mass / (p * p) +
+                                   1);
     // add mass tomorrow
   }
 }
@@ -32,13 +37,13 @@ void Sapphire::ParticleGamma<dim>::value_list(
   Assert(gammas.size() == points.size(),
          dealii::ExcDimensionMismatch(gammas.size(), points.size()));
   static_cast<void>(component);
-
+  ParticleProperties particle_properties;
   for (unsigned int i = 0; i < points.size(); ++i) {
     double p =
         (logarithmic_p ? std::exp(points[i][dim - 1]) : points[i][dim - 1]);
     gammas[i] = std::sqrt(
-        p * p + 1 / (reference_values.gamma * reference_values.gamma));
-    				// add mass tomorrow
+        p * p / (particle_properties.mass * particle_properties.mass) + 1.);
+    // add mass tomorrow
   }
 }
 
