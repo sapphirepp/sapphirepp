5d7d7a4ffface89f0a003048e1ad25ee1c3956f1
Nils Schween <nils.schween@mpi-hd.mpg.de>
2023-06-27 15:14:46 +0200

    fix: adapt reference values to the new unit system

--- include/vfp/reference-values.h
+++ include/vfp/reference-values.h
@@ -4,18 +4,10 @@
 namespace Sapphire {
 struct ReferenceValues {
   // reference values
-  const double energy = 10;       // GeV
-  const double mass = 0.9382721;  // GeV/c^2 (proton mass)
-  const double gamma = energy / mass;
+  const double mass = 1.672621923e-27;            // kg (proton mass)
   const double velocity = 299792458;              // m/s (speed of light)
   const double magnetic_field_strength = 1.e-10;  // Tesla (1 microGauss)
   const double charge = 1.602176634e-19;          // Coulmb ( elementary charge)
-  // NOTE: For the computation of the reciprocal of the gyro frequency, mass
-  // needs to be converted GeV/c^2 to kg. Hence, the factor 1.e9 *
-  // e/c^2
-  const double time = (gamma * mass * 1.602176634e-19) /
-                      (299792458. * 299792458. * charge *
-                       magnetic_field_strength);  // s (1/gyro_frequency)
 };
 
 template <typename StreamType>
--- src/vfp/reference-values.cpp
+++ src/vfp/reference-values.cpp
@@ -8,19 +8,16 @@ template <typename StreamType>
 StreamType &Sapphire::operator<<(
     StreamType &os, const Sapphire::ReferenceValues &reference_values) {
   os << "Reference Values: \n"
-     << "	Energy: " << reference_values.energy << " GeV \n"
-     << "	Mass: " << reference_values.mass << " GeV/c^2 \n"
-     << "	Gamma: " << reference_values.gamma << "\n"
+     << "	Mass: " << reference_values.mass << " kg \n"
      << "	Velocity: " << reference_values.velocity << " m/s \n"
      << "	Magnetic field strength: "
      << reference_values.magnetic_field_strength << "T \n"
-     << "	Charge: " << reference_values.charge << "C \n"
-     << "	Time: " << reference_values.time << "s \n\n";
+     << "	Charge: " << reference_values.charge << "C \n";
   return os;
 }
 
 // explicit instantiation
-template std::ostream &Sapphire::operator<<(
-    std::ostream &, const Sapphire::ReferenceValues &);
+template std::ostream &Sapphire::operator<<(std::ostream &,
+                                            const Sapphire::ReferenceValues &);
 template dealii::ConditionalOStream &Sapphire::operator<<(
     dealii::ConditionalOStream &, const Sapphire::ReferenceValues &);
