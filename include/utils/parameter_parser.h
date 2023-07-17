301791e15c41caa2a7c2a2a2aa1f9cd16b17a2d5
Florian Schulze <florian.schulze@mpi-hd.mpg.de>
2023-07-17 20:48:31 +0200

    feat(utils): Add ParameterParser class

---
+++ include/utils/parameter_parser.h
@@ -0,0 +1,49 @@
+/**
+ * @file parameter_parser.h
+ * @author Florian Schulze (florian.schulze@mpi-hd.mpg.de)
+ * @brief Implement ParameterParser class
+ * @version 0.1
+ * @date 2023-07-17
+ */
+
+#ifndef SAPPHIREUTISL_PARAMETERPARSER_H
+#define SAPPHIREUTISL_PARAMETERPARSER_H
+
+#include <deal.II/base/parameter_handler.h>
+
+#include <string>
+
+namespace Sapphire {
+
+namespace Utils {
+enum class OutputFormat { vtu, pvtu, hdf5 };
+} // namespace Utils
+
+namespace Utils {
+using namespace dealii;
+
+class ParameterParser {
+public:
+  ParameterParser(const std::string &prm_file_name);
+
+  const ParameterHandler &get_parameter_handler() const { return prm; }
+
+  /**Output parameter*/
+  std::string out_results_path;
+  std::string out_simulation_id;
+  std::string out_output_path;
+  std::string out_base_file_name;
+  unsigned int out_n_digits_for_counter;
+  Sapphire::Utils::OutputFormat out_format;
+  unsigned int out_output_frequency;
+
+private:
+  void declare_parameters();
+  void parse_parameters();
+
+  ParameterHandler prm;
+};
+
+} // namespace Utils
+} // namespace Sapphire
+#endif
---
+++ src/utils/parameter_parser.cpp
@@ -0,0 +1,64 @@
+#include "parameter_parser.h"
+
+#include <iostream>
+
+Sapphire::Utils::ParameterParser::ParameterParser(
+    const std::string &prm_file_name) {
+  declare_parameters();
+  prm.parse_input(prm_file_name);
+  prm.print_parameters(std::cout, ParameterHandler::PRM);
+  parse_parameters();
+}
+
+void Sapphire::Utils::ParameterParser::declare_parameters() {
+  prm.enter_subsection("Output");
+  {
+    prm.declare_entry(
+        "Results folder", "./results", dealii::Patterns::Anything(),
+        "Path to the folder in which the simulation results will be stored. "
+        "Without a trailing slash.");
+    prm.declare_entry("Simulation identifier", "", dealii::Patterns::Anything(),
+                      "Name of the simulation run. It will be "
+                      "used to create a subdirectory "
+                      "in the results folder.");
+    prm.declare_entry("Base file name", "solution",
+                      dealii::Patterns::Anything(),
+                      "The base file name for the output files.");
+    prm.declare_entry("Number of digits for counter", "4",
+                      dealii::Patterns::Integer(0),
+                      "The number of digits used for the counter in the "
+                      "output file names.");
+    prm.declare_entry(
+        "Format", "vtu", dealii::Patterns::Selection("vtu|pvtu|hdf5"),
+        "The format in which the simulation output will be stored.");
+    prm.declare_entry("Output frequency", "1", dealii::Patterns::Integer(0),
+                      "The frequence at which output files will "
+                      "be written. (In units of time steps)");
+  } // subsection Output
+  prm.leave_subsection();
+}
+
+void Sapphire::Utils::ParameterParser::parse_parameters() {
+  std::string s;
+
+  prm.enter_subsection("Output");
+  {
+    out_results_path = prm.get("Results folder");
+    out_simulation_id = prm.get("Simulation identifier");
+    out_base_file_name = prm.get("Base file name");
+    out_n_digits_for_counter = prm.get_integer("Number of digits for counter");
+
+    s = prm.get("Format");
+    if (s == "vtu")
+      out_format = Sapphire::Utils::OutputFormat::vtu;
+    else if (s == "pvtu")
+      out_format = Sapphire::Utils::OutputFormat::pvtu;
+    else if (s == "hdf5")
+      out_format = Sapphire::Utils::OutputFormat::hdf5;
+    else
+      AssertThrow(false, ExcNotImplemented());
+
+    out_output_frequency = prm.get_integer("Output frequency");
+  } // subsection Output
+  prm.leave_subsection();
+}
