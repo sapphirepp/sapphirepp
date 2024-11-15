cac0bfe662c403c88d7ba6dd2429b54e037b044f
Florian Schulze <florian.schulze@mpi-hd.mpg.de>
2024-11-15 17:12:05 +0100

    test(lin-waves): Add 2d+3d tests

---
+++ tests/mhd/linear-waves/test-linear-waves-2d.cpp
@@ -0,0 +1,137 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2023 by the Sapphire++ authors
+//
+// This file is part of Sapphire++.
+//
+// Sapphire++ is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// Sapphire++ is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sapphire++. If not, see <https://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+
+/**
+ * @file test/mhd/linear-waves/test-linear-waves-2d.cpp
+ * @author Florian Schulze (florian.schulze@mpi-hd.mpg.de)
+ * @brief Implement 2d tests for linear-waves example
+ */
+
+#include <deal.II/base/mpi.h>
+#include <deal.II/base/parameter_handler.h>
+
+#include <mpi.h>
+
+#include "config.h"
+#include "mhd-parameters.h"
+#include "output-parameters.h"
+#include "sapphirepp-logstream.h"
+#include "test-run-mhd.h"
+
+
+
+const unsigned int dim = 2;
+
+
+
+int
+main(int argc, char *argv[])
+{
+  try
+    {
+      /** [Main function setup] */
+      using namespace sapphirepp;
+      using namespace MHD;
+      dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(argc,
+                                                                  argv,
+                                                                  1);
+
+      saplog.init(2);
+
+      std::string parameter_filename = "parameter.prm";
+      if (argc > 1)
+        parameter_filename = argv[1];
+
+      double max_L2_error = 1e-10;
+      if (argc > 2)
+        max_L2_error = std::stod(argv[2]);
+
+      dealii::ParameterHandler prm;
+      PhysicalParameters       physical_parameters(dim);
+      Utils::OutputParameters  output_parameters;
+      MHDParameters<dim>       mhd_parameters;
+
+      physical_parameters.declare_parameters(prm);
+      output_parameters.declare_parameters(prm);
+      mhd_parameters.declare_parameters(prm);
+
+      prm.parse_input(parameter_filename);
+
+      physical_parameters.parse_parameters(prm);
+      output_parameters.parse_parameters(prm);
+      mhd_parameters.parse_parameters(prm);
+      /** [Main function setup] */
+
+
+      /** [Copy MHD parameter] */
+      const unsigned int spacedim    = MHDEquations::spacedim;
+      physical_parameters.box_length = std::vector<double>(dim);
+      for (unsigned int d = 0; d < dim; ++d)
+        {
+          physical_parameters.box_length[d] =
+            std::abs(mhd_parameters.p1[d] - mhd_parameters.p2[d]);
+
+          AssertThrow((mhd_parameters.boundary_conditions[2 * d + 0] ==
+                       MHD::BoundaryConditionsMHD::periodic) &&
+                        (mhd_parameters.boundary_conditions[2 * d + 1] ==
+                         MHD::BoundaryConditionsMHD::periodic),
+                      dealii::ExcMessage("This example assumes periodic BC."));
+        }
+      /** [Copy MHD parameter] */
+
+      /** [Setup analytic solution] */
+      InitialConditionMHD<spacedim> analytic_solution(
+        physical_parameters, mhd_parameters.adiabatic_index);
+      /** [Setup analytic solution] */
+
+      return test_run_mhd(mhd_parameters,
+                          physical_parameters,
+                          output_parameters,
+                          analytic_solution,
+                          max_L2_error);
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl
+                << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  return 0;
+}
