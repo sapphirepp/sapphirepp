ef825a057047073bf04f5cd65fbab93735473e41
Florian Schulze <florian.schulze@mpi-hd.mpg.de>
2024-11-21 15:32:03 +0100

    feat(slope-limiter): Add slope-limiter.h/.cpp

---
+++ include/sapphirepp/mhd/slope-limiter.h
@@ -0,0 +1,76 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2023 by the Sapphire++ authors
+//
+// This file is part of Sapphire++.
+//
+// Sapphire++ is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// Sapphire++ is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sapphire++. If not, see <https://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+
+/**
+ * @file slope-limiter.h
+ * @author Florian Schulze (florian.schulze@mpi-hd.mpg.de)
+ * @brief Define utility functions related to slope limiting
+ */
+
+#ifndef MHD_SLOPELIMITER_H
+#define MHD_SLOPELIMITER_H
+
+#include <deal.II/base/tensor.h>
+
+#include <deal.II/lac/vector.h>
+
+#include <array>
+#include <vector>
+
+#include "mhd-equations.h"
+#include "mhd-flags.h"
+
+namespace sapphirepp
+{
+  namespace MHD
+  {
+
+    /**
+     * @namespace sapphirepp::MHD::SlopeLimiter
+     * @brief Namespace for functions related to slope limiting
+     */
+    namespace SlopeLimiter
+    {
+
+      /**
+       * @brief Calculate the minmod function of a list of values.
+       *
+       * The minmod function is defined as,
+       * \f[
+       *   {\rm minmod}(v_1, \dots, v_k) =
+       *   \begin{cases}
+       *     s \min(|v_1|, \dots, |v_k|)
+       *      & {\rm if} \quad s = {\rm sgn}(v_1) = \dots {\rm sgn}(v_k) \\
+       *     0
+       *      & {\rm otherwise}
+       *   \end{cases} \,.
+       * \f]
+       *
+       * @param values Vector of values \f$ (v_1, \dots, v_k) \f$.
+       * @return double The minmod function of the input values,
+       *                \f$ {\rm minmod}(v_1, \dots, v_k) \f$.
+       */
+      double
+      minmod(const std::vector<double> &values);
+    } // namespace SlopeLimiter
+  }   // namespace MHD
+} // namespace sapphirepp
+#endif
---
+++ src/mhd/slope-limiter.cpp
@@ -0,0 +1,46 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2023 by the Sapphire++ authors
+//
+// This file is part of Sapphire++.
+//
+// Sapphire++ is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// Sapphire++ is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sapphire++. If not, see <https://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+
+/**
+ * @file slope-limiter.cpp
+ * @author Florian Schulze (florian.schulze@mpi-hd.mpg.de)
+ * @brief Implement utility functions related to slope limiting
+ */
+
+#include "slope-limiter.h"
+
+#include <deal.II/base/exceptions.h>
+
+#include <cmath>
+
+
+
+double
+sapphirepp::MHD::SlopeLimiter::minmod(const std::vector<double> &values)
+{
+  auto [min_it, max_it] = std::minmax_element(values.begin(), values.end());
+  if ((*min_it) * (*max_it) < 0.0)
+    return 0.0;
+  else if (std::abs(*min_it) < std::abs(*max_it))
+    return *min_it;
+  else
+    return *max_it;
+}
