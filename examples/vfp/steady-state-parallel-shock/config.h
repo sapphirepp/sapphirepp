ea3549096271681c86791162ee82570999862314
Nils Schween <nils.schween@mpi-hd.mpg.de>
2025-03-04 17:34:23 +0100

    example(steady-state): give nu_0 a physical interpretation, i.e 1/h

--- examples/vfp/steady-state-parallel-shock/config.h
+++ examples/vfp/steady-state-parallel-shock/config.h
@@ -237,7 +237,7 @@ namespace sapphirepp
             /** [Scattering frequency] */
             // Bohm limit
             scattering_frequencies[q_index] =
-              prm.nu0 * std::exp(-points[q_index][1]);
+              prm.nu0 * prm.B0 * std::exp(-points[q_index][1]);
             /** [Scattering frequency] */
           }
       }
