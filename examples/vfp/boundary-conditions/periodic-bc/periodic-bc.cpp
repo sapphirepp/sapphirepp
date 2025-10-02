4fae5b1754d9cf5812a09993d1d354adab6e2d64
Florian Schulze <florian.schulze@mpi-hd.mpg.de>
2025-10-02 14:45:49 +0200

    doc: Add examples for boundary tests
    Co-authored-by: Nils Schween <nils.schween@mpi-hd.mpg.de>

---
+++ examples/vfp/boundary-conditions/periodic-bc/periodic-bc.cpp
@@ -0,0 +1,95 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2023 by the Sapphire++ authors
+//
+// This file is part of Sapphire++.
+//
+// Sapphire++ is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// Sapphire++ is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Sapphire++. If not, see <https://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+
+/**
+ * @file examples/vfp/boundary-conditions/periodic-bc/periodic-bc.cpp
+ * @author Nils Schween (nils.schween@mpi-hd.mpg.de)
+ * @brief Implement main function for periodic-bc example
+ */
+
+#include <deal.II/base/mpi.h>
+#include <deal.II/base/parameter_handler.h>
+
+#include <mpi.h>
+
+#include "config.h"
+#include "output-parameters.h"
+#include "sapphirepp-logstream.h"
+#include "vfp-parameters.h"
+#include "vfp-solver.h"
+
+
+
+int
+main(int argc, char *argv[])
+{
+  try
+    {
+      using namespace sapphirepp;
+      using namespace VFP;
+      dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(argc,
+                                                                  argv,
+                                                                  1);
+      saplog.init(argc, argv);
+
+      std::string parameter_filename = "parameter.prm";
+      if (argc > 1)
+        parameter_filename = argv[1];
+
+      dealii::ParameterHandler prm;
+      PhysicalParameters       physical_parameters;
+      Utils::OutputParameters  output_parameters;
+      VFPParameters<dimension> vfp_parameters(vfp_flags);
+
+      physical_parameters.declare_parameters(prm);
+      output_parameters.declare_parameters(prm);
+      vfp_parameters.declare_parameters(prm);
+
+      prm.parse_input(parameter_filename);
+
+      physical_parameters.parse_parameters(prm);
+      output_parameters.parse_parameters(prm);
+      vfp_parameters.parse_parameters(prm);
+
+      VFPSolver<dimension> vfp_solver(vfp_parameters,
+                                      physical_parameters,
+                                      output_parameters);
+      vfp_solver.run();
+    }
+  catch (std::exception &exc)
+    {
+      sapphirepp::saplog.print_error(exc);
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl;
+      std::cerr << "\n"
+                << "----------------------------------------------------"
+                << "\n"
+                << "Unknown exception!" << "\n"
+                << "Aborting!" << "\n"
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  return 0;
+}
